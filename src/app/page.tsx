c85e04add5a33a9070a99c967a801e80ab0a6a8c
Peyush Nuwal <piyushnawal19@gmail.com>
2025-07-17 21:32:55 +0530

    landing page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/landing/Hero";
 
 
 
@@ -5,9 +6,7 @@ export default function Home() {
 
   return (
     <main className="min-h-screen px-6 py-4   bg-[var(--bg-gradient)] dark:bg-foreground  text-foreground dark:text-background">
-      <div className=" mt-10 bg-white text-black dark:bg-black dark:text-white">
-        Hello dark mode
-      </div>
+       <Hero />
     </main>
   );
 }
---
+++ src/components/landing/Hero.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+const Hero = () => {
+  return (
+    <div className="min-h-screen flex flex-col  items-center  justify-center mt-20 lg:mt-0 gap-10 lg:gap-0 ">
+      {/*----left-side----  */}
+      <div className="h-[70vh]  flex flex-col justify-center ">
+        <h1 className="text-3xl lg:text-5xl text-accent font-medium leading-8 lg:leading-12 text-center">
+          Build Your GitHub README <br /> Like a Pro
+        </h1>
+        <h3 className="text-sm  lg:text-base text-gray-500 text-centerdark:text-background font-light mt-2 max-lg:max-w-[270px]">
+          Design, customize, and export stunning READMEs with zero Markdown
+          skills.
+        </h3>
+        <div className="flex mt-4 gap-4 justify-center">
+          <button className="btn btn-primary">
+            <Link href="/dashboard">Try it Now</Link>
+          </button>
+          <button className="btn btn-outline">
+            <a href="https://github.com/peyush-nuwal/readme-forge">
+              ⭐ Star on GitHub
+            </a>
+          </button>
+        </div>
+      </div>
+      {/*----right-side----  */}
+      <div className=" lg:w-9/10 h-full  ">
+        <Image
+          src="/dashboard.png"
+          alt="dashobard"
+          width={700}
+          height={500}
+          className="w-full h-full object-contain rounded-2xl   "
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
